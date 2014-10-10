a1d6aafb319e94cfd4475b74445dccda76c4c8a0
Minh Tue Vo <mvo@box.com>
2014-10-10 10:28:10 -0700

    adding problem 38

---
+++ problem38.py
@@ -0,0 +1,21 @@
+def is_pandigital(s):
+	if len(s) != 9:
+		return False
+	for i in range(1, 10):
+		if s.find(str(i)) == -1:
+			return False
+	return True
+
+m = 0
+base = -1
+for i in range(1, 500000):
+	s = ''
+	for j in range(1, 10):
+		s += str(i*j)
+		if len(s) == 9:
+			break
+	if len(s) == 9 and is_pandigital(s):
+		if int(s) > m:
+			m = int(s)
+			base = i
+			print base, m
