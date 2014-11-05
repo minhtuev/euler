fad115b45aa6713722d467b761d4aa9b565637a2
Minh Tue Vo <mvo@box.com>
2014-11-05 11:51:51 -0800

    adding problem 63

---
+++ problem63.py
@@ -0,0 +1,14 @@
+count = 0
+for n in range(1, 10):
+	index = 1
+	power = 1
+	while (True):
+		power = power * n
+		if len(str(power)) == index:
+			print n, index, power
+			count += 1
+		if power <= 10**(index - 3 ):
+			break
+		index += 1
+
+print "total count:", count
