0ae6967b3a566168840e33bc8cf1f2aeb00904cc
Minh Tue Vo <mvo@box.com>
2014-11-13 19:09:12 -0800

    adding problem 68

--- problem68.py
+++ problem68.py
@@ -27,24 +27,20 @@ class EqualityConstraint(Constraint):
 		self.nodes = nodes
 		self.compared_nodes = compared_nodes
 	
-	def value(self):
-		return sum(nodes)
-
-	def is_set(self):
-		return self.value() != int('inf')
-
 	def assert_constraint(self):
-		if not(self.is_set() and self.compared_nodes.is_set()):
+		if sum(self.nodes) == float('inf') or sum(self.compared_nodes) == float('inf'):
 			return None
-		return self.value == self.compared_nodes.value
+		return sum(self.nodes) == sum(self.compared_nodes)
 
-class LessThanConstraint(Constraint):
+class GreaterThanConstraint(Constraint):
 	def __init__(self, node, compared_node):
 		self.node = node
 		self.compared_node = compared_node
 
 	def assert_constraint(self):
-		return self.node.value < self.compared_node.value
+		if self.node.value == float('inf') or self.compared_node.value == float('inf'):
+			return None
+		return self.node.value > self.compared_node.value
 
 class UniqueConstraint(Constraint):
 	def __init__(self, nodes):
@@ -52,33 +48,44 @@ class UniqueConstraint(Constraint):
 
 	def assert_constraint(self):
 		L = []
-		for node in nodes:
+		for node in self.nodes:
 			if node.value in L:
 				return False
 			elif node.value != float('inf'):
 				L.append(node.value)
 		return True
 
-def validate_constraint(constraints):
+def check_constraint(constraints):
 	for constraint in constraints:
-		if constraint.assert_constraint == False:
+		if constraint.assert_constraint() == False:
+			#print constraint.__class__
+			#print constraint.nodes
+			#print constraint.compared_nodes
 			return False
 	return True
 
-def compute(L, nodes, values, constraints, index = 0):
+def validate_constraint(constraints):
+	for constraint in constraints:
+		if constraint.assert_constraint() != True:
+			return False
+	return True
+
+def compute(L, nodes, values, constraints, index = 0, max_number = -1):
+	#print L
 	if index == len(L):
 		order = ['a', 'b', 'c', 'd', 'c', 'f', 'e', 'f', 'g', 'h', 'g', 'j', 'i', 'j', 'b']
-		S = ''
-		for ch in order:
-			S += str(nodes[ch]) + " "
-		if len(S) == 16:
+		S = [str(nodes[ch]) for ch in order]
+		S = ''.join(S)
+		print S
+		if len(S) == 16 and int(S) > max_number:
 			print S
+			max_number = int(S)
 	else:
 		for value in values:
 			V = values[:]
 			V.remove(value)
 			L[index].value = value
-			if validate_constraint(constraints):
+			if check_constraint(constraints):
 				compute(L, nodes, V, constraints, index + 1)
 		L[index].reset()
 
@@ -88,27 +95,30 @@ def get_node_list(dic, name_list):
 		nodes.append(dic[name])
 	return nodes
 
-nodes = {}
-L = []
-for node_name in list(map(chr, range(97, 107))): #or list(map(chr, range(ord('a'), ord('z')+1))):
-	nodes[node_name] = Node(ord(node_name))
-	L.append(nodes[node_name])
+def main():	
+	nodes = {}
+	L = []
+	for node_name in list(map(chr, range(97, 107))): #or list(map(chr, range(ord('a'), ord('z')+1))):
+		nodes[node_name] = Node()
+		L.append(nodes[node_name])
 
-abc = get_node_list(nodes, 'abc')
-dcf = get_node_list(nodes, 'dcf')
-efg = get_node_list(nodes, 'efg')
-hgj = get_node_list(nodes, 'hgj')
-ijb = get_node_list(nodes, 'ijb')
+	abc = get_node_list(nodes, 'abc')
+	dcf = get_node_list(nodes, 'dcf')
+	efg = get_node_list(nodes, 'efg')
+	hgj = get_node_list(nodes, 'hgj')
+	ijb = get_node_list(nodes, 'ijb')
 
-constraints = []
-# constraints.append(UniqueConstraint('abcdefghij'))
-constraints.append(EqualityConstraint(dcf, abc))
-constraints.append(EqualityConstraint(efg, abc))
-constraints.append(EqualityConstraint(hgj, abc))
-constraints.append(EqualityConstraint(ijb, abc))
-constraints.append(LessThanConstraint(nodes['d'], nodes['a']))
-constraints.append(LessThanConstraint(nodes['e'], nodes['a']))
-constraints.append(LessThanConstraint(nodes['h'], nodes['a']))
-constraints.append(LessThanConstraint(nodes['i'], nodes['a']))
+	constraints = []
+	# constraints.append(UniqueConstraint('abcdefghij'))
+	constraints.append(EqualityConstraint(dcf, abc))
+	constraints.append(EqualityConstraint(efg, abc))
+	constraints.append(EqualityConstraint(hgj, abc))
+	constraints.append(EqualityConstraint(ijb, abc))
+	constraints.append(GreaterThanConstraint(nodes['d'], nodes['a']))
+	constraints.append(GreaterThanConstraint(nodes['e'], nodes['a']))
+	constraints.append(GreaterThanConstraint(nodes['h'], nodes['a']))
+	constraints.append(GreaterThanConstraint(nodes['i'], nodes['a']))
 
-compute(L, nodes, range(1, 11), constraints)
+	compute(L, nodes, range(1, 11), constraints)
+
+main()
