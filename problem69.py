bad2fc0a42ba85c9097d45a6aaa686ab213cfbc4
Minh Tue Vo <mvo@box.com>
2014-11-15 11:24:41 -0800

    adding problem 69

--- problem69.py
+++ problem69.py
@@ -1,4 +1,5 @@
-from numlib import gcd
+from numlib import gcd, get_prime_factors
+from sets import Set
 
 def totient(n):
 	count = 0
@@ -7,11 +8,37 @@ def totient(n):
 			count += 1
 	return count
 
+# this requires another approach to the problem
+# maybe i need to compute the prime factor set of each number
+# and then quickly find the intersection between two prime sets using Set datastructur
+
+def totient2(n, dic):
+	count = 0
+	for i in range(2, n):
+		if len(dic[n].intersection(dic[i])) == 0:
+			count += 1
+	return count + 1
+
+def totient3(n):
+	L = get_prime_factors(n)
+	count = float(n)
+	for f in L:
+		count = count*(1 - 1.0/f)
+	return count
+
 def main():
 	max_value = 0
+	dic = {}
+	primes = []
 	for n in range(2, 1000001):
-		if float(n)/totient(n) > max_value:
-			max_value = float(n)/totient(n)
-			print n, totient(n), max_value
+		L = get_prime_factors(n)
+		dic[n] = Set()
+		for f in L:
+			dic[n].add(f)
+		if float(n)/totient2(n, dic) > max_value:
+			max_value = float(n) / totient2(n, dic)
+			print n, totient2(n, dic), max_value
 
+print get_prime_factors(1298)
+print get_prime_factors(649)
 main()
