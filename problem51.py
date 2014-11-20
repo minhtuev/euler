2df05d6d8a222e4870d25ed24173b2e777b4f257
Minh Tue Vo <mvo@box.com>
2014-11-20 13:11:22 -0800

    problem 51

--- problem51.py
+++ problem51.py
@@ -1,12 +1,10 @@
-prime_list = [2]
-n = 3
-dic = {}
+from numlib import is_prime
 
 def get_digit_positions(n1, n2):
 	n1 = str(n1)
 	n2 = str(n2)
 	if len(n1) != len(n2):
-		return None
+		return []
 	L = []
 	for index in range(len(n1)):
 		if n1[index] != n2[index]:
@@ -20,40 +18,34 @@ def same_digit(n, L):
 			return False
 	return True
 
-num_prime = 8
-has_found = False
-dic = {}
-dic[2] = {}
-check = {}
-finger = 0
-while (not has_found):
-	is_prime = True
-	for prime in prime_list:
-		if n % prime == 0:
-			is_prime = False
-			break
-		if prime**2 >= n:
-			break
-	if is_prime:
-		check[n] = {}
-		dic[n] = {}
-		i = finger
-		while (i < len(prime_list)):
-			prime = prime_list[i]
-			positions = get_digit_positions(n, prime)
-			if positions != None and not(positions in check):
-				if same_digit(prime, positions) and same_digit(n, positions):
-					dic[prime][positions] = dic[prime].get(positions, 1) + 1
-					check[n][positions] = 1
-					if dic[prime][positions] == num_prime:
-						print "the smallest prime found with the desired property is:", prime
-						has_found = True
-						break
-				elif positions == None:
-					# all the positions before should be invalid. do not consider anymore.
-					finger = i + 1
-			i += 1
-		# adding to the growing prime list
-		prime_list.append(n)
-		print n
-	n += 1
+def main():
+	num_prime = 8
+	dic = {}
+	prime_list = []
+	n = 2
+	while (True):
+		if is_prime(n):
+			check = {}
+			temp = []
+			for prime in prime_list:
+				positions = get_digit_positions(prime, n)
+				if len(positions) > 0 and not(positions in check) and same_digit(n, positions) and same_digit(prime, positions):
+					dic[prime][positions] = dic[prime].get(positions, [])
+					dic[prime][positions].append(n)
+					if len(dic[prime][positions]) + 1 == num_prime:
+						print prime, dic[prime][positions], positions
+						return
+					check[positions] = True
+				# prunning the search space
+				if len(str(prime)) != len(str(n)):
+					del dic[prime]
+				else:
+					temp.append(prime)
+			dic[n] = {}
+			prime_list = temp
+			# adding to the growing prime list
+			prime_list.append(n)
+			print n, prime_list[0]
+		n += 1
+
+main()
