35c2db596fab1fad0e57e292f6c48e4b106f7150
Minh Tue Vo <mvo@box.com>
2014-12-15 00:10:27 -0800

    adding problem 11

--- problem11.py
+++ problem11.py
@@ -16,23 +16,47 @@ def main():
 	# going horizontally
 	for row in A:
 		product = 1
-		for i in range(m):
-			product *= row[i]
+		for col in range(m):
+			product *= row[col]
 		max_number = max(max_number, product)
-		for i in range(1, n - m):
-			product = product * row[i+n-1]/row[i - 1]
+		for col in range(1, n - m):
+			if row[col - 1] != 0:
+				product = product * row[col+m-1]/row[col - 1]
+			else:
+				product = 1
+				for new_col in range(col, col+m):
+					product *= row[new_col]
 			max_number = max(max_number, product)
 	# going vertically
 	for col in range(n):
 		product = 1
-		for i in range(m):
-			product *= row[i][col]
+		for row in range(m):
+			product *= A[row][col]
 		max_number = max(max_number, product)
 		for row in range(1, n - m):
-			product = product * A[row+n-1][col]/A[row - 1][col]
+			if A[row - 1][col] != 0:
+				product = product * A[row+m-1][col]/A[row - 1][col]
+			else:
+				product = 1
+				for new_row in range(row, row+m):
+					product *= A[new_row][col]
+			max_number = max(max_number, product)
+			
+	# going diagonally in one direction
+	for row in range(0, n - m):
+		for col in range(0, n - m):
+			product = 1
+			for index in range(m):
+				product *= A[row + index][col + index]
 			max_number = max(max_number, product)
 
+	for row in range(m - 1, n):
+		for col in range(m - 1, n):
+			product = 1
+			for index in range(m):
+				product *= A[row - index][col - index]
+			max_number = max(max_number, product)
 
-
+	print "max number", max_number
 
 main()
