0640bd5e50806c0b28123dc99e7abf47e2fe00dc
Minh Tue Vo <mvo@box.com>
2014-12-23 15:53:14 -0800

    adding problem 85

--- problem85.py
+++ problem85.py
@@ -11,4 +11,18 @@ def count_square(w, h):
 	else:
 		return count_square(w, h - 1) + count_square(w - 1, h ) + h*w - count_square(w - 1, h - 1)
 
-print count_square(3, 2)
+def main():
+	h = 1
+	w = 1
+	target = 2000000
+	while count_square(w, h) < target:
+		w += 1
+	max_range = w
+	max_number = 0
+	for h in range(1, max_range):
+		for w in range(1, max_range):
+			if count_square(w, h) < target and count_square(w, h) > max_number:
+				max_number = count_square(w, h)
+				print max_number, w, h
+
+main()
