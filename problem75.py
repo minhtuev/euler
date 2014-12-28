e1824ebf95cd828e001b90460709664239c15062
Minh Tue Vo <mvo@box.com>
2014-12-27 16:55:29 -0800

    adding problem 75

--- problem75.py
+++ problem75.py
@@ -1,16 +1,16 @@
 from numlib import is_square
 
 def main():
-	max_range = 500000
+	max_range = 500
 	dic = {}
 	checked = {}
 	max_L = max_range*3
 	for a in range(1, max_range + 1):
 		b = a + 1
-		while (b <= max_range):
+		c2 = a*a + b*b
+		c = int(c2**0.5)
+		while (b <= max_range) and (a + b + c <= max_L):
 			if not ((a, b) in checked):
-				c2 = a*a + b*b
-				c = int(c2**0.5)
 				if c2 == c*c:
 					s = a + b + c
 					k = 1
@@ -19,13 +19,14 @@ def main():
 						checked[(a*k, b*k)] = True
 						print a*k, b*k, (s - a - b)*k
 						k +=1
-			if a + b  +c > max_L:
-				break
 			b += 2
+			c2 = a*a + b*b
+			c = int(c2**0.5)
+			
 
 	count = 0
 	for key in dic:
-		if dic[key] == 1 and key <= max_L:
+		if dic[key] == 1:
 			count += 1
 	print "count:", count
 
